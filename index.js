badb300dcfe2d4b1f41d92e1281d5ec7c440bcad
Wong Siow Wei <b022210249@student.utem.edu.my>
2025-01-23 20:58:50 +0800

    wait

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ const { message } = require("statuses");
 //Setup mongodb
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = process.env.MONGO_URL;
-const credentials =
-  "D:\\UTEM\\sem 5\\BERR3123 INFORMATION SECURITY\\Assignment\\MyCERT\\X509-cert-6182322632771355153.pem";
+const credentials = process.env.MONGO_CERT_PATH;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
