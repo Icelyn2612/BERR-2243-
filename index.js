197fda37e84f0b686f7f2e578c66e147c0344837
Wong Siow Wei <b022210249@student.utem.edu.my>
2025-01-28 13:18:52 +0800

    waa

--- index.js
+++ index.js
@@ -652,7 +652,7 @@ app.post("/userLogin", async (req, res) => {
   const verifyHuman = await verifyRecaptchaToken(
     req.body['g_recaptcha_response']
   );
-  if (!verifyHuman) {
+  if (verifyHuman) {
     return res
       .status(400)
       .send("reCAPTCHA verification failed. Please try again.");
